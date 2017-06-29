7ab0f8ba557695173b48daafde46e0cdd458cd0f
AndriiMoravskyi <andriy_moravskyi@macminissu10.training.local>
2017-06-29 17:29:48 +0300

    Initial Commit

---
+++ CaLculator_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CaLculator_1
+//
+//  Created by Andriy_Moravskyi on 6/29/17.
+//  Copyright © 2017 Andriy_Moravskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
